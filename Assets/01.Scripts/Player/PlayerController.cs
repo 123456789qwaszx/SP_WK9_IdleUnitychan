04e7ecc130504631fa0e4e17af87a883d1d8bc87
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-09 01:55:28 +0900

    Fix : Move애니메이션 폴리싱
    1. 처음 좌우 이동이 안먹던건, dir y의 예외처리가 이상해서, else로 빠졌기 때문.
    2. 그걸 수정했더니, 위를 볼땐 dir/ 아래를 볼땐 -dir을 하는 과정에서 고개가 확확 튀어버림.
    3. 그걸 수정하기 위해선, 우선 animation Blending을 해야하고
    4. 스크립트에서도 Lerp를 사용해야 하는데
    이번기회에 Lerp의 조건을 다시 깨달음.
    (float a,float b,float c)라고 적혀있지만
    a는 무조건 a= Lerp(a, ...)으로 해야 실시간으로 반영됨. 만약 a=Lerp(0.2, ...)로 하면 a= 0.2로 한것과 똑같음.
    
    5. 마찬가지로 조건도 좌우 Dir이 확 뒤집힐때는
    최대한 0에 가깝도록 해야 자연스러움.
    _curDirx = Mathf.Lerp(_curDirx, 0, Time.deltaTime * 0.5f);
    이렇게.
    
    6. 추가로 아직 안한것인데,
    if (dir.X ...) 조건으로 정확한 애니메이션 방향을 잡아줘야 조금 더 정확하고 예쁜 애니메이션을 만들 수 있음.

--- Assets/01.Scripts/Player/PlayerController.cs
+++ Assets/01.Scripts/Player/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour, IDamageable
     CharacterController controller;
     IDamageable idamagable;
 
-    public float _moveSpeed;
+    public float _moveSpeed = 1.7f;
     public float _jumpPower;
     public float _rotationSensitivety = 1300f;
 
@@ -23,6 +23,7 @@ public class PlayerController : MonoBehaviour, IDamageable
     public bool _readyToJump;
     public bool CanAct = true;
 
+    float _curDirx;
 
     public float _gravity;
     public float _verticalSpeed;
@@ -72,36 +73,45 @@ public class PlayerController : MonoBehaviour, IDamageable
     {
         Vector3 dir = GameManager.Instance.MouseDir;
         Vector3 moveDir = new Vector3(dir.x, 0, dir.y).normalized;
-
-        _moveSpeed = Mathf.Abs(dir.y) + Mathf.Abs(dir.x) + Mathf.Abs(dir.z);
         anim.speed = 1.5f;
 
 
-        if (dir.y > 0.1)
+        if (dir.y > 0.2)
         {
             controller.Move(moveDir * Time.deltaTime * _moveSpeed * 2.3f);
 
             Quaternion lookRotation = Quaternion.LookRotation(moveDir);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * _rotationSensitivety);
 
-            anim.SetFloat(_HashMoveSpeed, dir.y);
-            anim.SetFloat(_HashMoveDirection, dir.x);
+            anim.SetFloat(_HashMoveSpeed, dir.y + Mathf.Abs(dir.x));
+            _curDirx = Mathf.Lerp(_curDirx, -1, Time.deltaTime * 0.5f);
+            anim.SetFloat(_HashMoveDirection, _curDirx);
+            Debug.Log(_curDirx);
         }
-        else if (dir.y < -0.1)
+        else if (dir.y < -0.2)
         {
             controller.Move(moveDir * Time.deltaTime * _moveSpeed * 2.3f);
 
             Quaternion lookRotation = Quaternion.LookRotation(moveDir);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * _rotationSensitivety);
 
-            anim.SetFloat(_HashMoveSpeed, Mathf.Abs(dir.y));
-            anim.SetFloat(_HashMoveDirection, -dir.x);
+            anim.SetFloat(_HashMoveSpeed, Mathf.Abs(dir.y) + Mathf.Abs(dir.x));
+            _curDirx = Mathf.Lerp(_curDirx, 1, Time.deltaTime * 0.5f);
+            anim.SetFloat(_HashMoveDirection, _curDirx);
+            Debug.Log(_curDirx);
         }
-        else
+        else if (dir.y >= -0.2 && dir.y <= 0.2 && dir.y != 0)
         {
-            controller.Move(moveDir * Time.deltaTime * Mathf.Lerp(_moveSpeed, 0.01f, 120.0f * Time.deltaTime));
+            controller.Move(moveDir * Time.deltaTime * _moveSpeed * 1.7f);
 
-            anim.SetFloat(_HashMoveSpeed, Mathf.Lerp(Mathf.Abs(dir.y), 0.01f, 3.0f * Time.deltaTime));
+            Quaternion lookRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, Time.deltaTime * _rotationSensitivety);
+
+            anim.SetFloat(_HashMoveSpeed, Mathf.Abs(dir.y) + Mathf.Abs(dir.x));
+            _curDirx = Mathf.Lerp(_curDirx, 0, Time.deltaTime * 0.5f);
+            anim.SetFloat(_HashMoveDirection, _curDirx);
+            Debug.Log(_curDirx);
+
         }
 
         if (moveDir == Vector3.zero)
@@ -162,3 +172,21 @@ public class PlayerController : MonoBehaviour, IDamageable
         }
     }
 }
+
+//
+// 1. 처음 좌우 이동이 안먹던건, dir y의 예외처리가 이상해서, else로 빠졌기 때문.
+// 2. 그걸 수정했더니, 위를 볼땐 dir/ 아래를 볼땐 -dir을 하는 과정에서 고개가 확확 튀어버림.
+// 3. 이걸 수정하기 위해선,
+//   1) 우선 animation Blending을 해야하고
+//   2) 스크립트에서도 Lerp를 사용해야 하는데
+// 이번기회에 Lerp의 조건을 다시 깨달음.
+// (float a,float b,float c)라고 적혀있지만
+// a는 무조건 a= Lerp(a, ...)으로 해야 실시간으로 반영됨. 만약 a=Lerp(0.2, ...)로 하면 a= 0.2로 한것과 똑같음.
+
+//   3)마찬가지로 조건도 좌우 Dir이 확 뒤집힐때는
+// 최대한 0에 가깝도록 해야 자연스러움.
+// _curDirx = Mathf.Lerp(_curDirx, 0, Time.deltaTime * 0.5f);
+// 이렇게.
+
+//  4)추가로 아직 안한것인데,
+// if (dir.X ...) 조건으로 정확한 애니메이션 방향을 잡아줘야 조금 더 정확하고 예쁜 애니메이션을 만들 수 있음.
