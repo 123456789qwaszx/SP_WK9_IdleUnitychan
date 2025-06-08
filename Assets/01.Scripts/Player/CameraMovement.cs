c884e08fd39e35ff83b42723414b4d06787d6c6f
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-08 23:52:15 +0900

    Fix : CameraMovement 추가
    카메라 회전, tilt 기능 삭제
    Lerp로 부드럽게 따라가도록 변경.

--- Assets/01.Scripts/Player/CameraMovement.cs
+++ Assets/01.Scripts/Player/CameraMovement.cs
@@ -18,21 +18,6 @@ public class CameraMovement : MonoBehaviour
 
     void LateUpdate()
     {
-        // if (lastdir == Vector3.zero)
-        // {
-            // Vector3 dir = GameManager.Instance.MouseDir;
-            // Vector3 moveDir = new Vector3(dir.x, 0, dir.y);
-
-            transform.position =_target.position + _offset;
-
-            //lastdir = dir;
-        // }
-        // else
-        // {
-        //     Vector3 lastdir = GameManager.Instance.MouseDir;
-        //     Vector3 moveDir = new Vector3(lastdir.x, 0, lastdir.y);
-
-        //     transform.position = -moveDir + _target.position + _offset;
-        // }
+        transform.position = Vector3.Lerp(transform.position, _target.position + _offset, 2.0f * Time.deltaTime);
     }
 }
