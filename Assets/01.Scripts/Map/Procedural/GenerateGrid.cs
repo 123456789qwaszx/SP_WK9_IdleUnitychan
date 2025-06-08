01fa02ee7d7eb942ae9ed66ee585c549de21ec0b
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-08 14:06:54 +0900

    Study : 절차적맵생성_2.맵생성시 받은 위치값을 토대로 object 생성
    절차적맵생성_2.맵생성시 받은 위치값을 토대로 object 생성
    
    1. 리스트를 만들어서, Chunk의 위치값을 싹다 넣고,
    그 위치값 중 랜덤하게 뽑아 물체를 생성시킨다. 생성 후, 물체가 생성된 인덱스는 제거해, 중복생성을 막는다.

--- Assets/01.Scripts/Map/Procedural/GenerateGrid.cs
+++ Assets/01.Scripts/Map/Procedural/GenerateGrid.cs
@@ -6,6 +6,8 @@ public class GenerateGrid : MonoBehaviour
 {
     public GameObject blockGameObject;
 
+    public GameObject objectToSpawn;
+
     private int worldSizeX = 10;
 
     private int worldSizeZ = 10;
@@ -14,19 +16,52 @@ public class GenerateGrid : MonoBehaviour
 
     private float gridOffset = 1.1f;
 
+    private List<Vector3> blockPositions = new List<Vector3>();
+
     void Start()
     {
         for (int x = 0; x < worldSizeX; x++)
         {
             for (int z = 0; z < worldSizeZ; z++)
             {
-                Vector3 pos = new Vector3(x * gridOffset, generateNoise(x,z,8f) * noiseHeight, z * gridOffset);
+                Vector3 pos = new Vector3(x * gridOffset, generateNoise(x, z, 8f) * noiseHeight, z * gridOffset);
 
-                GameObject block = Instantiate(blockGameObject, pos, Quaternion.identity) as GameObject;
+                //GameObject block = Instantiate(blockGameObject, pos, Quaternion.identity) as GameObject;
+                GameObject block = PoolManager.Instance.Pop(blockGameObject);
+                block.transform.position = pos;
+                block.transform.rotation = Quaternion.identity;
 
-                block.transform.SetParent(this.transform);
+                blockPositions.Add(block.transform.position);
+
+                //block.transform.SetParent(this.transform);
             }
         }
+        SpawnObject();
+    }
+
+    private void SpawnObject()
+    {
+        for (int c = 0; c < 20; c++)
+        {
+            GameObject toPlaceObject = PoolManager.Instance.Pop(objectToSpawn);
+            toPlaceObject.transform.position = ObjectSpawnLocation();
+            toPlaceObject.transform.rotation =  Quaternion.identity;
+
+        }
+    }
+
+    private Vector3 ObjectSpawnLocation()
+    {
+        int rndIndex = Random.Range(0, blockPositions.Count);
+        Vector3 newPos = new Vector3(
+            blockPositions[rndIndex].x,
+            blockPositions[rndIndex].y + 0.5f,
+            blockPositions[rndIndex].z
+        );
+
+        blockPositions.RemoveAt(rndIndex);
+        return newPos;
+
     }
 
     private float generateNoise(int x, int z, float detailScale)
