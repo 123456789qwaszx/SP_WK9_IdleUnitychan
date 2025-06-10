3781dc7b57815a829d59fb900afc8cf55447730c
123456789qwaszx <64720058+123456789qwaszx@users.noreply.github.com>
2025-06-10 14:53:50 +0900

    Add : FindVisibleTarget() 추가
    시야 범위 내, 모든 collider들을 가져오고,
    특정 레이어를 가졌다면, List에 추가.
    
    나중에 이 List.Count 만큼 적을 찾아가 공격하는 기능을 추가하면 될듯함.

--- Assets/01.Scripts/Player/EnemyScanner.cs
+++ Assets/01.Scripts/Player/EnemyScanner.cs
@@ -4,13 +4,81 @@ using UnityEngine;
 
 public class EnemyScanner : MonoBehaviour
 {
+    public float heightOffset = 0.0f;
     public float detectionRadius = 10;
     [Range(0.0f, 360.0f)]
     public float detectionAngle = 360;
+    public float maxHeightDifference = 1.0f;
     public LayerMask viewBlockerLayerMask;
     public Dectable _enemy;
 
-    public Dectable DetectEnemy(Player player)
+    public float ViewAngle;    //시야각
+    public float ViewDistance; //시야거리
+
+    public List<Transform> detected = new List<Transform>();
+
+    void Update()
+    {
+        DetectEnemy();
+        DrawView();
+        FinaVisibleTargets();
+        Debug.Log(detected.Count);
+    }
+
+
+    public void FinaVisibleTargets()
+    {
+        // 시야범위 내에서 MonsterLayer를 가진 콜라이더 가져오기
+        // 이러면 저 시야 범위를 5~10으로 정할 수도 있나? 그러면 그걸로 바닥을 판단해서 지워버리면 진짜 편할 것 같은데 나중에 해봐야겠다.
+        int _mask = 1 << (int)Layer.Monster;
+        Collider[] targets = Physics.OverlapSphere(gameObject.transform.position, ViewDistance, _mask);
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            Transform target = targets[i].transform;
+
+            // 타겟 까지의 단위벡터
+            Vector3 dirToTarget = (target.position - gameObject.transform.position).normalized;
+
+            // 시야에 들어왔는지 체크
+            if (Vector3.Dot(gameObject.transform.position, dirToTarget) > Mathf.Cos((ViewAngle / 2) * Mathf.Deg2Rad))
+            {
+                float distToTarget = Vector3.Distance(gameObject.transform.position, target.position);
+
+                if (Physics.Raycast(gameObject.transform.position, dirToTarget, distToTarget, _mask))
+                {
+                    detected.Add(target);
+                }
+            }
+        }
+    }
+
+
+    public void DrawView()
+    {
+        Vector3 leftBoundary = DirFromAngle(-ViewAngle / 2);
+        Vector3 rightBoundary = DirFromAngle(ViewAngle / 2);
+        Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + leftBoundary * ViewDistance, Color.green);
+        Debug.DrawLine(gameObject.transform.position, gameObject.transform.position + rightBoundary * ViewDistance, Color.green);
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees)
+    {
+        // 좌우 회전값 갱신
+        angleInDegrees += transform.eulerAngles.y;
+        // 경계 벡터값 반환
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+
+    public void DrawBoundary(GameObject go, Dectable dectable)
+    {
+        Vector3 detectionRange = go.transform.position - dectable.transform.position;
+
+        if (detectionRange.sqrMagnitude <= detectionRadius * detectionRadius) ;
+    }
+
+    public Dectable DetectEnemy()
     {
         Vector3 toEnemy = _enemy.transform.position - CharacterManager.Instance.Player.transform.position;
 
@@ -21,17 +89,17 @@ public class EnemyScanner : MonoBehaviour
         if (toEnemy.sqrMagnitude <= detectionRadius * detectionRadius)
         {
             // 대박. 역시 수학을 잘하니 편하구나. 이렇게 내적을 구하는 거구나
-            if (Vector3.Dot(toEnemy.normalized, player.transform.forward) >
+            if (Vector3.Dot(toEnemy.normalized, CharacterManager.Instance.Player.transform.forward) >
                 // 내적 값이, 시야각/2의 Cos값보다 큰지 체크 
                 Mathf.Cos(detectionAngle * 0.5f * Mathf.Deg2Rad))
             {
                 bool canSee = false;
 
-                Debug.DrawRay(CharacterManager.Instance.Player.transform.position, toEnemy, Color.blue);
+                //Debug.DrawRay(CharacterManager.Instance.Player.transform.position, toEnemy, Color.blue);
 
                 canSee |= !Physics.Raycast(CharacterManager.Instance.Player.transform.position, toEnemy.normalized, detectionRadius,
                         viewBlockerLayerMask, QueryTriggerInteraction.Ignore);
-                        
+
                 if (canSee)
                     return _enemy;
             }
